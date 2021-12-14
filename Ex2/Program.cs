9d6e3527220f556e3c527f146d0a9876377aef03
KIRUYXAN <279901@gmail.com>
2021-12-14 21:33:40 +0300

    .

---
+++ Ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введи число");
+double a = Convert.ToDouble(Console.ReadLine());
+int z = 0;
+double b = a;
+while (b > 0)
+{
+    z++;
+    b /= 10;
+}
+Console.WriteLine($"В числе {a}: {z} цифр");
---
+++ Ex3/Program.cs
@@ -0,0 +1,8 @@
+﻿int s = 0;
+int a = 0;
+do
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a > 0 && a % 2 > 0) s += a;
+} while (a != 0);
+Console.WriteLine($"Сумма:  {s}");
