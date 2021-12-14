797c057505a5ebec529fe7c60a96d7c5404534ac
KIRUYXAN <279901@gmail.com>
2021-12-14 17:36:24 +0300

    .

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введи первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()); ;
+Console.WriteLine("Введи второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+    if (a < c) Console.WriteLine($"{a} - минимальнное число");
+    else Console.WriteLine($"{c} - минимальнное число");
+else
+    if (b < c) Console.WriteLine($"{b} - минимальнное число");
+    else Console.WriteLine($"{c} - минимальнное число");
