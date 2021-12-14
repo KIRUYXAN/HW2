65b1cca8fb39c4d31ad1f6ecc1e9ce1d28b5e0cb
KIRUYXAN <279901@gmail.com>
2021-12-14 22:51:22 +0300

    .

---
+++ Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿int err = 3;
+bool a;
+do
+{
+    Console.WriteLine("Введите логин");
+    string log = Console.ReadLine();
+    Console.WriteLine("Введите пароль");
+    string passw = Console.ReadLine();
+    if (log == "root" && passw == "GeakBrains")
+    {
+        a = true;
+        Console.WriteLine(a);
+        err = 0;
+    }
+    else
+    {
+        a = false;
+        Console.WriteLine(a);
+        err--;
+        Console.WriteLine($"У вас осталось {err} попытки");
+        Console.WriteLine("");
+    }
+} while (err != 0);
+if (a == false) 
+    Console.WriteLine("Вы заблокированы");
