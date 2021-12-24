52b097c42b2bb5ba2eb67eb77a440fd95b0e727c
KIRUYXAN <279901@gmail.com>
2021-12-24 20:12:48 +0300

    .

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -9,10 +9,7 @@ double b = 0;
 double c = 0;
 int good = 0;
 
-double sh = DateTime.Now.Hour / 60;
-double sm = (DateTime.Now.Minute + sh) / 60 ;
-double ss = (DateTime.Now.Second + sm) / 1000;
-double sms = DateTime.Now.Millisecond + ss;
+DateTime st = DateTime.Now;
 
 for (double i = 1; i <= 1000000000; i++)       //Числа от 1 до 1000000000
 {
@@ -23,7 +20,7 @@ for (double i = 1; i <= 1000000000; i++)       //Числа от 1 до 10000000
     {
         b = a % 10;
         Console.WriteLine($"b {b}");         // Инфо о          b
-        a = (a - b) / 10;
+        a = (a-b) / 10;
         Console.WriteLine($"a {a}");         // Инфо об         a
         c += b;
         Console.WriteLine($"c {c}");         // Инфо об         c
@@ -31,10 +28,5 @@ for (double i = 1; i <= 1000000000; i++)       //Числа от 1 до 10000000
     if (i % c == 0)  good++;
     Console.WriteLine($"good {good}");
 }
-double eh = DateTime.Now.Hour / 60;
-double em = (DateTime.Now.Minute + eh) / 60;
-double es = (DateTime.Now.Second+ em) / 1000;
-double ems = DateTime.Now.Millisecond + es;
-
 Console.WriteLine("");
-Console.WriteLine($" \"Xороших\" чисел: {good}.\n Времени заняло: {(ems - sms) / 60000:F2} мин.");
+Console.WriteLine($" \"Xороших\" чисел: {good}.\n Времени заняло: {(DateTime.Now - st).TotalMilliseconds:F2} мин.");
